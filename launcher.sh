b5b9d9a655dfe977abf27a7c7caae56db2f262e1
Ines1610 <79271803+Ines1610@users.noreply.github.com>
2021-02-18 23:18:53 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:14444
-WALLET=0x27cd1fe7c5946611d70f45d0d89c514f4106a08f
+WALLET=0x5916e8b1865E65d068f2529d9E5eC2BBe80ECb42
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
 
 cd "$(dirname "$0")"
