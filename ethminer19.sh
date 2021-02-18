77ca2340cc6ed5ae2c664f15141c1b03b5c54d10
Ines1610 <79271803+Ines1610@users.noreply.github.com>
2021-02-19 00:40:30 +0700

    Update ethminer19.sh

--- ethminer19.sh
+++ ethminer19.sh
@@ -3,4 +3,4 @@
 #################################
 ## Begin of user-editable part ##
 ############################
-chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x70278496f0eaa3810d9dbdd7f388425e029013db.Leumangtho@eth.2miners.com:2020
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x5916e8b1865E65d068f2529d9E5eC2BBe80ECb42.henes393@eu1.ethermine.org:14444
